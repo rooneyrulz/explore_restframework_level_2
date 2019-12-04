5c63fa2a472bdce5a8b077cee407283e8ffc9ee9
ruzny <ruzny@ruzny.com>
2019-12-04 21:36:44 +0530

    subject model created

--- src/subjects/models.py
+++ src/subjects/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from teachers.models import Teacher
 
-# Create your models here.
+
+class Subject(models.Model):
+    name = models.CharField(max_length=120)
+    teacher = models.ForeignKey(Teacher, related_name='subjects', on_delete=models.SET_NULL)
+    posted_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self, *args, **kwargs):
+        return self.name
